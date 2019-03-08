84daf5044aaae9daf78478960858c6b0f6c0dbd2
Luis Bigas <dev@lbigas.com>
2019-03-08 23:35:07 +0000

    st

--- suckless/st-0.8.2/config.h
+++ suckless/st-0.8.2/config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 /* static char *font = "Inconsolata:pixelsize=17:antialias=true:autohint=true"; */
-static char *font = "Hack:pixelsize=15:antialias=true:autohint=true";
+static char *font = "Inconsolata:pixelsize=18:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
