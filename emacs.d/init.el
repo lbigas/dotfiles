d665d3bb6d75374f40ec9f49471a809d7b677dd6
Luis Bigas <dev@lbigas.com>
2020-07-06 16:46:21 +0100

    emacs config

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -3,7 +3,7 @@
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
-(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
+;; (setq gnutls-algorithm-priority "NORMAL:-VERS-TBS1.3")
 
 ;; Bootstrap `use-package`
 (unless (package-installed-p 'use-package)
@@ -13,6 +13,12 @@
 ;; (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
 
 
+;; ispell
+(add-hook 'org-mode-hook 'flyspell-mode)
+(setq ispell-dictionary "portugues") ;default language dictionary
+;; (add-hook 'org-mode-hook 'ispell)
+
+
 ;; Interface Tweaks
 
 (setq inhibit-startup-message t)
@@ -20,8 +26,13 @@
 (toggle-scroll-bar -1)
 (fset 'yes-or-no-p 'y-or-n-p)
 
+;; Display Current column number
+(setq column-number-mode t)
+
 ;; Set Line numbers
-(global-display-line-numbers-mode)
+(add-hook 'prog-mode-hook 'linum-mode)
+;; (setq linum-format "%4d \u2502 ")
+;; (global-display-line-numbers-mode)
 
 ;; Scroll fix
 (setq redisplay-dont-pause t
@@ -57,15 +68,15 @@
 ;; (use-package color-theme-sanityinc-tomorrow
 ;;   :ensure t)
 
-(use-package modus-operandi-theme
-  :ensure t)
-
-;; (use-package faff-theme
+;; (use-package modus-operandi-theme
 ;;   :ensure t)
 
-;; (load-theme 'faff t)
+(use-package faff-theme
+  :ensure t)
 
-(load-theme 'modus-operandi t)
+(load-theme 'faff t)
+
+;; (load-theme 'modus-operandi t)
 
 ;; try a package without installing
 (use-package try
@@ -83,10 +94,10 @@
 
 (use-package counsel
   :ensure t
-  :bind
-  (("M-y" . counsel-yank-pop)
-   :map ivy-minibuffer-map
-   ("M-y" . ivy-next-line))
+  ;; :bind
+  ;; (("M-y" . counsel-yank-pop)
+  ;;  :map ivy-minibuffer-map
+  ;;  ("M-y" . ivy-next-line))
   )
 
 (use-package ivy
@@ -95,24 +106,25 @@
   :bind (("C-x b" . ivy-switch-buffer))
   :config
   (ivy-mode 1)
-  (setq ivy-use-virtual-buffers t)
+  ;; (setq ivy-use-virtual-buffers t)
   (setq ivy-count-format "%d/%d ")
   (setq ivy-display-style 'fancy))
 
 (use-package swiper
   :ensure try
   :bind(("C-s" . swiper-isearch)
-        ("C-r" . swiper-isearch)
+        ("C-r" . swiper-isearch-backward)
         ("C-c C-r" . ivy-resume)
         ("M-x" . counsel-M-x)
         ("C-x C-f" . counsel-find-file))
-  :config
-  (progn
-    (ivy-mode 1)
-    (setq ivy-use-virtual-buffers t)
-    (setq enable-recursive-minibuffers t)
-    (define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
-    ))
+  ;; :config
+  ;; (progn
+  ;;   (ivy-mode 1)
+  ;;   ;; (setq ivy-use-virtual-buffers t)
+  ;;   (setq enable-recursive-minibuffers t)
+  ;;   (define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
+  ;; ))
+  )
 
 (use-package avy
   :ensure t
@@ -127,56 +139,62 @@
   (global-set-key (kbd "C--") 'er/contract-region)
   )
 
-(use-package smartparens
-  :ensure t)
+(use-package wrap-region
+  :ensure t
+  :config
+  (wrap-region-global-mode t)
+  )
 
-(bind-keys
- :map smartparens-mode-map
- ("C-M-a" . sp-beginning-of-sexp)
- ("C-M-e" . sp-end-of-sexp)
+;; (use-package smartparens
+;;   :ensure t)
 
- ("C-<down>" . sp-down-sexp)
- ("C-<up>"   . sp-up-sexp)
- ("M-<down>" . sp-backward-down-sexp)
- ("M-<up>"   . sp-backward-up-sexp)
+;; (bind-keys
+;;  :map smartparens-mode-map
+;;  ("C-M-a" . sp-beginning-of-sexp)
+;;  ("C-M-e" . sp-end-of-sexp)
 
- ("C-M-f" . sp-forward-sexp)
- ("C-M-b" . sp-backward-sexp)
+;;  ("C-<down>" . sp-down-sexp)
+;;  ("C-<up>"   . sp-up-sexp)
+;;  ("M-<down>" . sp-backward-down-sexp)
+;;  ("M-<up>"   . sp-backward-up-sexp)
 
- ("C-M-n" . sp-next-sexp)
- ("C-M-p" . sp-previous-sexp)
+;;  ("C-M-f" . sp-forward-sexp)
+;;  ("C-M-b" . sp-backward-sexp)
 
- ("C-S-f" . sp-forward-symbol)
- ("C-S-b" . sp-backward-symbol)
+;;  ("C-M-n" . sp-next-sexp)
+;;  ("C-M-p" . sp-previous-sexp)
 
- ("C-<right>" . sp-forward-slurp-sexp)
- ("M-<right>" . sp-forward-barf-sexp)
- ("C-<left>"  . sp-backward-slurp-sexp)
- ("M-<left>"  . sp-backward-barf-sexp)
+;;  ("C-S-f" . sp-forward-symbol)
+;;  ("C-S-b" . sp-backward-symbol)
 
- ("C-M-t" . sp-transpose-sexp)
- ("C-M-k" . sp-kill-sexp)
- ("C-k"   . sp-kill-hybrid-sexp)
- ("M-k"   . sp-backward-kill-sexp)
- ("C-M-w" . sp-copy-sexp)
- ("C-M-d" . delete-sexp)
+;;  ("C-<right>" . sp-forward-slurp-sexp)
+;;  ("M-<right>" . sp-forward-barf-sexp)
+;;  ("C-<left>"  . sp-backward-slurp-sexp)
+;;  ("M-<left>"  . sp-backward-barf-sexp)
 
- ("M-<backspace>" . backward-kill-word)
- ("C-<backspace>" . sp-backward-kill-word)
- ([remap sp-backward-kill-word] . backward-kill-word)
+;;  ("C-M-t" . sp-transpose-sexp)
+;;  ("C-M-k" . sp-kill-sexp)
+;;  ("C-k"   . sp-kill-hybrid-sexp)
+;;  ("M-k"   . sp-backward-kill-sexp)
+;;  ("C-M-w" . sp-copy-sexp)
+;;  ("C-M-d" . delete-sexp)
 
- ("M-[" . sp-backward-unwrap-sexp)
- ("M-]" . sp-unwrap-sexp)
+;;  ("M-<backspace>" . backward-kill-word)
+;;  ("C-<backspace>" . sp-backward-kill-word)
+;;  ([remap sp-backward-kill-word] . backward-kill-word)
 
- ("C-x C-t" . sp-transpose-hybrid-sexp)
+;;  ("M-[" . sp-backward-unwrap-sexp)
+;;  ("M-]" . sp-unwrap-sexp)
 
- ("C-c ("  . wrap-with-parens)
- ("C-c ["  . wrap-with-brackets)
- ("C-c {"  . wrap-with-braces)
- ("C-c '"  . wrap-with-single-quotes)
- ("C-c \"" . wrap-with-double-quotes)
- ("C-c _"  . wrap-with-underscores)
- ("C-c `"  . wrap-with-back-quotes))
+;;  ("C-x C-t" . sp-transpose-hybrid-sexp)
+
+;;  ("C-c ("  . wrap-with-parens)
+;;  ("C-c ["  . wrap-with-brackets)
+;;  ("C-c {"  . wrap-with-braces)
+;;  ("C-c '"  . wrap-with-single-quotes)
+;;  ("C-c \"" . wrap-with-double-quotes)
+;;  ("C-c _"  . wrap-with-underscores)
+;;  ("C-c `"  . wrap-with-back-quotes))
 
 ;; easier project buffer/file management
 (use-package projectile
@@ -262,6 +280,13 @@
 
 (global-set-key (kbd "C-c c") 'hydra-avy/body)
 
+
+;; Org-Mode
+(use-package org-bullets
+  :ensure t
+  :config
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -273,10 +298,10 @@
    (vector "#ffffff" "#c82829" "#718c00" "#eab700" "#4271ae" "#8959a8" "#3e999f" "#4d4d4c"))
  '(beacon-color "#c82829")
  '(blink-cursor-mode nil)
- '(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
+ '(custom-enabled-themes (quote (faff)))
  '(custom-safe-themes
    (quote
-    ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" default)))
+    ("b54dc0500ff61167e1201b0dba6925f354a267f51b4d4b15e338c2d67d92fbda" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" default)))
  '(fci-rule-color "#d6d6d6")
  '(flycheck-color-mode-line-face-to-color (quote mode-line-buffer-id))
  '(flymake-error-bitmap
@@ -310,7 +335,7 @@
  '(ibuffer-title-face (quote dired-header))
  '(package-selected-packages
    (quote
-    (undo-tree magit expand-region which-key try use-package)))
+    (faff-theme undo-tree magit expand-region which-key try use-package)))
  '(show-paren-mode t)
  '(tool-bar-mode nil)
  '(vc-annotate-background nil)
@@ -336,11 +361,7 @@
      (340 . "#eab700")
      (360 . "#718c00"))))
  '(vc-annotate-very-old-color nil)
- '(window-divider-mode nil)
- '(xterm-color-names
-   ["#000000" "#a60000" "#005e00" "#813e00" "#0030a6" "#721045" "#00538b" "#f0f0f0"])
- '(xterm-color-names-bright
-   ["#505050" "#972500" "#315b00" "#70480f" "#223fbf" "#8f0075" "#30517f" "#ffffff"]))
+ '(window-divider-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
