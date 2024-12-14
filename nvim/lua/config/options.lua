eefe0da21f3e1d3b35091a7ddb5db6c3ad26a643
Luis Bigas <dev@lbigas.com>
2024-12-14 20:13:01 +0000

    initial nvim config

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,31 @@
+vim.opt.guicursor = ""
+
+-- line numbers
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.o.tabstop = 2 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 2 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 2 -- Number of spaces inserted when indenting
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+-- clipboard
+vim.opt.clipboard:append("unnamedplus") -- use system clipboard
+
+vim.opt.updatetime = 50
+
+vim.opt.signcolumn = 'yes' -- leave space for the gutter
---
+++ nvim/lua/config/remap.lua
@@ -0,0 +1,15 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("n", "<esc>", ":noh <CR>") -- clear last search highlight
+
+-- window management
+vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
+vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
+vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+
+vim.keymap.set("n", "<C-h>", "<C-w>h", {desc = "Go to Left Window", remap = true})
+vim.keymap.set("n", "<C-j>", "<C-w>j", {desc = "Go to Lower Window", remap = true})
+vim.keymap.set("n", "<C-k>", "<C-w>k", {desc = "Go to Upper Window", remap = true})
+vim.keymap.set("n", "<C-l>", "<C-w>l", {desc = "Go to Right Window", remap = true})
