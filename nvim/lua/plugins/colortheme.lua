bd475e2bdbac5950f94fede630d1ca223bf7796c
Luis Bigas <dev@lbigas.com>
2024-12-23 08:33:35 +0000

    didsable copilot

--- nvim/lua/plugins/colortheme.lua
+++ nvim/lua/plugins/colortheme.lua
@@ -1,7 +1,14 @@
 return {
-	"rose-pine/neovim",
-	name = "rose-pine",
-	config = function()
-		vim.cmd("colorscheme rose-pine-main")
-	end
+  "miikanissi/modus-themes.nvim",
+  priority = 1000,
+  config = function()
+    vim.cmd([[colorscheme modus_vivendi]])
+  end
 }
+-- return {
+-- 	"rose-pine/neovim",
+-- 	name = "rose-pine",
+-- 	config = function()
+-- 		vim.cmd("colorscheme rose-pine-main")
+-- 	end
+-- }
