07c1d9d20920cfdab8a40497d3e50ea4cd2d66e1
Luis Bigas <dev@lbigas.com>
2024-12-30 09:16:09 +0000

    vim fugitive keybinds

--- nvim/lua/plugins/vim-fugitive.lua
+++ nvim/lua/plugins/vim-fugitive.lua
@@ -1,3 +1,7 @@
 return {
-  'tpope/vim-fugitive'
+  'tpope/vim-fugitive',
+  config = function()
+    vim.keymap.set('n', 'gh', '<cmd>diffget //2<cr>', { desc = 'Get the hunk in the left' })
+    vim.keymap.set('n', 'gl', '<cmd>diffget //3<cr>', { desc = 'Get the hunk in the right' })
+  end
 }
