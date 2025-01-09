af1662ae2b8931779c66808183d2e05e5f6dd37d
Luis Bigas <dev@lbigas.com>
2025-01-09 17:06:50 +0000

    nvim: surround and scratch plugins

---
+++ nvim/lua/plugins/scratch.lua
@@ -0,0 +1,8 @@
+return {
+  "LintaoAmons/scratch.nvim",
+  event = "VeryLazy",
+  config = function()
+    vim.keymap.set("n", "<M-C-n>", "<cmd>Scratch<cr>")
+    vim.keymap.set("n", "<M-C-o>", "<cmd>ScratchOpen<cr>")
+  end
+}
