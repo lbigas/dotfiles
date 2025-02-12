53980958dc491fbd21b8d5523b1af6957f06dc0e
Luis Bigas <dev@lbigas.com>
2025-02-12 18:35:59 +0000

    nvim using mini plugins

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -7,10 +7,10 @@ return {
     -- calling `setup` is optional for customization
     local builtin = require("fzf-lua")
     builtin.setup({})
-    vim.keymap.set('n', '<leader>ff', builtin.git_files, { desc = 'fzf find git files' })
-    vim.keymap.set('n', '<leader>pf', builtin.files, { desc = 'fzf find files' })
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'fzf live grep' })
-    vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'fzf buffers' })
-    vim.keymap.set('n', '<leader>fh', builtin.helptags, { desc = 'fzf help tags' })
+    vim.keymap.set('n', '<leader>p', builtin.git_files, { desc = 'fzf find git files' })
+    vim.keymap.set('n', '<leader>f', builtin.files, { desc = 'fzf find files' })
+    vim.keymap.set('n', '<leader>g', builtin.live_grep, { desc = 'fzf live grep' })
+    vim.keymap.set('n', '<leader>b', builtin.buffers, { desc = 'fzf buffers' })
+    vim.keymap.set('n', '<leader>h', builtin.helptags, { desc = 'fzf help tags' })
   end
 }
