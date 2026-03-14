86a8b39ca6396383986be919bdbfacc6f650aa93
Luis Bigas <dev@lbigas.com>
2026-03-14 22:09:40 +0000

    Improve WezTerm selection visibility with explicit highlight colors

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,6 +3,10 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.color_scheme = 'rose-pine'
+config.colors = {
+  selection_bg = '#9ccfd8',
+  selection_fg = '#191724',
+}
 config.font = wezterm.font('JetBrains Mono NL')
 config.font_size = 11.0
 config.hide_tab_bar_if_only_one_tab = true
