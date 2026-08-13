2c56388b64a37398f0df080cf5ef7ed36c3f8f86
Luis Bigas <dev@lbigas.com>
2026-08-13 12:32:35 +0100

    feat(nvim): use fzf instead of telescope

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -6,15 +6,18 @@ return {
   "ibhagwan/fzf-lua",
   -- optional for icon support
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  enabled = false,
+  enabled = true,
   config = function()
-    -- calling `setup` is optional for customization
-    local builtin = require("fzf-lua")
-    builtin.setup({})
-    vim.keymap.set('n', '<leader>p', builtin.git_files, { desc = 'fzf find git files' })
-    vim.keymap.set('n', '<leader>f', builtin.files, { desc = 'fzf find files' })
-    vim.keymap.set('n', '<leader>g', builtin.live_grep, { desc = 'fzf live grep' })
-    vim.keymap.set('n', '<leader>b', builtin.buffers, { desc = 'fzf buffers' })
-    vim.keymap.set('n', '<leader>h', builtin.helptags, { desc = 'fzf help tags' })
+    local fzf = require("fzf-lua")
+    fzf.setup({})
+
+    vim.keymap.set('n', '<leader>p', fzf.git_files, { desc = 'fzf find git files' })
+    vim.keymap.set('n', '<leader>f', fzf.files, { desc = 'fzf find files' })
+    vim.keymap.set('n', '<leader>g', fzf.live_grep, { desc = 'fzf live grep' })
+    vim.keymap.set('n', '<leader>b', fzf.buffers, { desc = 'fzf buffers' })
+    vim.keymap.set('n', '<leader>h', fzf.helptags, { desc = 'fzf help tags' })
+    vim.keymap.set('n', '<leader>ss', fzf.lsp_document_symbols, { desc = 'fzf document symbols' })
+    vim.keymap.set('n', '<leader>sS', fzf.lsp_workspace_symbols, { desc = 'fzf workspace symbols' })
+    vim.keymap.set('n', '<leader><leader>', fzf.oldfiles, { desc = 'fzf recent files' })
   end
 }
